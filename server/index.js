eee9041032ae7c38378e8f133617ceb9710e3a5f
rajnish98 <rajnishpandey63@gmail.com>
2024-04-20 20:33:12 +0530

    updated server logiic

--- server/index.js
+++ server/index.js
@@ -13,12 +13,16 @@ const server = http.createServer((req, res) => {
 
   if (req.url == '/') {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end();
-
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Welcome to node js server by rajnish Pandey");
   } else if (req.url == '/about'){
-    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("About Page");
   } else if (req.url == '/contact'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Contact Page");
     
   } else if (req.url == '/prosuct'){
     res.statusCode = 200;
